d3883e165c77602649d55a6e40e39eb62b1022da
Jordan Koplowicz <jordan@koplowicz.com>
2019-05-28 11:03:00 -0700

    Added CORB warning.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
 document.getElementById("submitButton").onclick = function(){
+    // Get query.
     let query = document.getElementById("query").value;
-    let result = 'Your query was ' + query;
-    document.getElementById("result").innerHTML = result;
+
+    // Set up query to Wikipedia api.
+    let url = "https://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=10&gapfrom=Ba&prop=links%7Ccategories&format=json";
+
+    fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            console.log('Checkout this JSON! ', out);
+        })
+        .catch(err => { throw err });
+
+    // Calculate the results.
+
+    // Paste results onto the page.
+    document.getElementById("result").innerHTML = url;
 };
